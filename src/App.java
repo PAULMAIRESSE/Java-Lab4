f709ec67b8f9324dc119a66b0185d8c5e50fd381
P4UL-M <paul.mairesse@free.fr>
2023-10-02 15:49:12 -0400

    Comment

--- src/App.java
+++ src/App.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a list of numbers separated by spaces: ");
-        String userInput = scanner.nextLine();
-        scanner.close();
+        Scanner scanner = new Scanner(System.in); // Create a Scanner object
+        System.out.println("Enter a list of numbers separated by spaces: "); // Ask user input
+        String userInput = scanner.nextLine(); // Read user input
+        scanner.close(); // Close scanner
         try {
-            double average = Calculator.averageFromUserInput(userInput);
-            System.out.println("The average is: " + average);
+            double average = Calculator.averageFromUserInput(userInput); // Call averageFromUserInput method
+            System.out.println("The average is: " + average); // Print average
         } catch (InputFormatException e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.getMessage()); // Print error message
             return;
         }
     }
--- src/Calculator.java
+++ src/Calculator.java
@@ -3,20 +3,22 @@ import java.util.ArrayList;
 public class Calculator {
 
     public static double averageFromUserInput(String userInput) throws InputFormatException {
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        ArrayList<Integer> numbers = new ArrayList<Integer>(); // Create an ArrayList object
         try {
-            for (String number : userInput.split(" ")) {
-                numbers.add(Integer.parseInt(number));
+            for (String number : userInput.split(" ")) { // Split user input by spaces and loop through each number
+                numbers.add(Integer.parseInt(number)); // Add number to ArrayList
             }
-        } catch (NumberFormatException e) {
+        } catch (NumberFormatException e) { // Catch NumberFormatException if user input is not a number
             throw new InputFormatException("Input must be a list of numbers separated by spaces.");
         }
 
+        // Calculate sum
         int total = 0;
         for (int number : numbers) {
             total += number;
         }
 
+        // Return average
         return total / numbers.size();
     }
 }
